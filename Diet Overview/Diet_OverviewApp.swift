daf6c27fe502e73f96b08e84087d8fcc83e0f0ce
Jakob Wolf <jakob-wolf14@live.dk>
2024-12-23 17:03:57 +0100

    Initial Commit

---
+++ Diet Overview/Diet_OverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diet_OverviewApp.swift
+//  Diet Overview
+//
+//  Created by dmu mac 31 on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Diet_OverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
